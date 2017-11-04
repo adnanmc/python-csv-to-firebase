280ab1e0d1d23ce960c1dff32f9877ba8fda53db
Chance Smith <chancesmithb@gmail.com>
2017-11-04 15:55:00 -0500

    adds data duplication prevition

--- start.py
+++ start.py
@@ -7,7 +7,17 @@ firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
 # get data from csv
+# loop through csv
 data = {"name": "Mortimer 'Morty' Smiths"}
 
 # push to firebase
-db.child("user").push(data)
+# db.child("user").push(data)
+users = db.child("user").order_by_child("name").equal_to('Jimm').get()
+
+# check if user exists
+if users.each():
+  print('yep')
+else:
+  print('nope')
+
+# send data up to firebase
